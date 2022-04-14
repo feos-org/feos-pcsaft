1367124ef2fc8cd00b5cb42a1212fa7e7929515f
Philipp Rehner <69816385+prehner@users.noreply.github.com>
2022-04-14 10:08:36 +0200

    Fix clippy lints (#37)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![warn(clippy::all)]
 #![allow(clippy::too_many_arguments)]
-#![allow(clippy::many_single_char_names)]
-#![allow(clippy::suspicious_operation_groupings)]
-
 mod dft;
 mod eos;
 mod parameters;
